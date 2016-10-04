a5b01b2288bc87b65a49bdbfc517bf92424791d2
biyeah <bfymail@126.com>
2016-10-04 14:38:38 -0400

    third

--- polls/views.py
+++ polls/views.py
@@ -1,38 +1,36 @@
 from django.shortcuts import render
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponseRedirect
 from django.shortcuts import get_object_or_404
 from django.urls import reverse
+from django.views import generic
 # from django.template import loader
 
 from .models import Question, Choice
 
-def index(request):
-	# return HttpResponse("Hello world ,haha")
-	latest_question_list = Question.objects.order_by('-pub_date')[:5]
-	# output = ', '.join([q.question_text for q in latest_question_list])
-	# template = loader.get_template('polls/index.html')
-	context = {
-		'latest_question_list': latest_question_list,
-	}
-	# return HttpResponse(output)
-	# return HttpResponse(template.render(context, request))
-	return render(request, 'polls/index.html', context)
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
 
-def detail(request, question_id):
-	# return HttpResponse("You are looking at question %s" % question_id)
-	question = get_object_or_404(Question, pk=question_id)
-	return render(request, 'polls/detail.html', {'question': question})
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return Question.objects.order_by('-pub_date')[:5]
 
-def results(request, question_id):
-	question = get_object_or_404(Question, pk=question_id)
-	return render(request, 'polls/results.html', {'question': question})
+
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
+
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
 
 def vote(request, question_id):
 	question = get_object_or_404(Question, pk=question_id)
 	try:
 		selected_choice = question.choice_set.get(pk=request.POST['choice'])
 	except (KeyError, Choice.DoesNotExist) as e:
-		raise e
+		# raise e
 		return render(request, 'polls/detail.html',{
 			'question': question,
 			'error_message': "You didn't select a choice"
